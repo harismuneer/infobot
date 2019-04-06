9582551827b0f1bdc7fb29dd5e0f5d274158da3b
MahamAmjad <38891160+MahamAmjad@users.noreply.github.com>
2019-04-06 20:37:06 +0500

    Update Merge_Index.py

--- code/index/Merge_Index.py
+++ code/index/Merge_Index.py
@@ -1,64 +1,56 @@
-import os
-
-
-i1 = []
-i2 = []
-
-
 def merge_index(index1, index2):           # merges two index files
-    with open(index1, 'r') as file:
-        holder1 = file.read()
 
-    with open(index2, 'r') as file:
-        holder2 = file.read()
-
-    i1 = holder1.split("\n\n")
-    i1 = i1[0:len(i1)-1]
-
-    i2 = holder2.split("\n\n")
-    i2 = i2[0:len(i2)-1]
-
-    path = os.getcwd()
-    path = path + "\merged_index.txt"
+    path = "merged_index.txt"
     merge = open(path, "w+", encoding='latin-1', errors='ignore')
-
-    index = 0
-
-    while index < len(i1) and index < len(i2):
-        if i1[index].split(",")[0] == i2[index].split(",")[0]:
-            key = i1[index].split(",")[0]
-            document_frequency = int(i1[index].split(",")[1]) + int(i2[index].split(",")[1])
-            temp1 = i1[index][i1[index].find(','):]
-            temp1 = temp1[i1[index].find(','):]
-            temp2 = i2[index][i2[index].find(','):]
-            temp2 = temp2[i2[index].find(','):]
-            if int(i1[index].split(",")[2]) < int(i2[index].split(",")[2]):
-                posting_list = temp1 + temp2
+    with open(index1, 'r') as file1, open(index2, 'r') as file2:
+        r_buffer1 = file1.readline()
+        r_buffer2 = file2.readline()
+        while r_buffer1 and r_buffer2:
+            if r_buffer1.split(",")[0] == r_buffer2.split(",")[0]:
+                key = r_buffer1.split(",")[0]
+                document_frequency = int(r_buffer1.split(",")[1]) + int(r_buffer2.split(",")[1])
+                temp1 = r_buffer1[r_buffer1.find(','):]
+                temp1 = temp1[r_buffer1.find(','):]
+                temp2 = r_buffer2[r_buffer2.find(','):]
+                temp2 = temp2[r_buffer2.find(','):]
+                while r_buffer1[len(r_buffer1)-1] is not '\n':
+                    r_buffer1 = file1.readline()
+                    temp = r_buffer1[r_buffer1.find(','):]
+                    temp = temp[r_buffer1.find(','):]
+                    temp1 = temp1 + temp
+                temp1 = temp1[: len(temp1)-1]
+                while r_buffer2[len(r_buffer2)-1] is not '\n':
+                    r_buffer2 = file2.readline()
+                    temp = r_buffer2[r_buffer2.find(','):]
+                    temp = temp[r_buffer2.find(','):]
+                    temp2 = temp2 + temp
+                temp2 = temp2[: len(temp2)-1]
+                posting = temp1 + temp2
+                merge.write(key+","+str(document_frequency)+posting+"\n")
             else:
-                posting_list = temp2 + temp1
-            merge.write(key+","+str(document_frequency)+posting_list+"\n\n")
+                keys = sorted([r_buffer1.split(",")[0], r_buffer2.split(",")[0]])
+                if keys[0] == r_buffer1.split(",")[0]:
+                    merge.write(r_buffer1)
+                    while r_buffer1[len(r_buffer1)-1] is not '\n':
+                        r_buffer1 = file1.readline()
+                        merge.write(r_buffer1)
+                else:
+                    merge.write(r_buffer2)
+                    while r_buffer2[len(r_buffer2)-1] is not '\n':
+                        r_buffer2 = file2.readline()
+                        merge.write(r_buffer2)
+            r_buffer1 = file1.readline()
+            r_buffer2 = file2.readline()
+        while r_buffer1:
+            merge.write(r_buffer1)
+            r_buffer1 = file1.readline()
 
-        else:
-            keys = sorted([i1[index].split(",")[0], i2[index].split(",")[0]])
-            if keys[0] == i1[index].split(",")[0]:
-                merge.write(i1[index]+"\n\n")
-            else:
-                merge.write(i2[index]+"\n\n")
-
-        index += 1
-
-    while index < len(i1):
-        merge.write(i1[index]+"\n\n")
-        index += 1
-
-    while index < len(i2):
-        merge.write(i2[index]+"\n\n")
-        index += 1
-
-    file.close()
+        while r_buffer2:
+            merge.write(r_buffer2)
+            r_buffer2 = file2.readline()
     return
 
 
-''' Take 2 indexes file at a time to merger them into a single file'''
+# Take 2 indexes file at a time to merger them into a single file
 merge_index("index_1.txt", "index_2.txt")
 merge_index("merged_index.txt", "index_3.txt")
